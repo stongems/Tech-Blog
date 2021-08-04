58de2027c1cbdd6dfa0433aa1756cdb51f952f23
St.Onge <stonge.ms@gmail.com>
2021-08-03 21:27:21 -0700

    fixed login bug

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -7,14 +7,13 @@ router.post('/', async (req, res) => {
   const userData = await 
     User.create(req.body);
 
+  
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-
-   
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
     });
-
-  res.redirect("/")
   // } catch (err) {
   //   res.status(400).json(err);
   // }
